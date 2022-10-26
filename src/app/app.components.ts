e9036ac84b9aa6562f70a0400c2684e471244947
Daniil <62614735+DaniilTimonov@users.noreply.github.com>
2022-10-26 17:18:49 +0300

    прохождение

--- src/app/app.components.ts
+++ src/app/app.components.ts
@@ -5,12 +5,17 @@ import { Component } from '@angular/core';
     
     selector: 'my-app',
     template: `
-    <div class="mainbanner"></div>
-    <child-comp></child-comp>
- 
 
+    <child-comp  [userName]="name" [userAge]="age"    
+    (onChanged)="onChanged($event)"
+    >
+    <div class="mainbanner"></div>
+   
                 <p>Привет {{name}}</p>
-              
+                </child-comp>   
+                <input type="text" [(ngModel)]="name" />    
+                <input type="number" [(ngModel)]="age" />           
+                <h2>Количество кликов: {{clicks}}</h2>
                 `,
     styles: [`h2, p {color:#333;}`,
     `.mainbanner {width:100%; height:100px; background: teal;}
@@ -18,5 +23,11 @@ import { Component } from '@angular/core';
     `]
 })
 export class AppComponent { 
-    name = 'Петр';
+    name:string="Tom";
+    age:number = 24;
+
+    clicks:number = 0;
+    onChanged(increased:any){
+        increased==true?this.clicks++:this.clicks--;
+    }
 }
